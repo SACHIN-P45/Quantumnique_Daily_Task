72aa75580e9999ad44ffca267afd56853f0f0902
Sachin P <sachinabi67@gmail.com>
2026-03-24 16:06:00 +0530

    Add files via upload

---
+++ DAY-8/intersection.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class intersection
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr1[]=new int[n];
+        int arr2[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr1[i]=sc.nextInt();
+            arr2[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr1[i]==arr2[j])
+                {
+                    System.out.print(arr1[i]+" ");
+                }
+            }
+        }
+    }
+}
---
+++ DAY-8/selectionSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class seletionSort
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int min=arr[i];
+            int min_index=i;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[j]<min)
+                {
+                    min=arr[j];
+                    min_index=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min_index];
+            arr[min_index]=temp;
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
